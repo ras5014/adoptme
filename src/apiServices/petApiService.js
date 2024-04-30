3f8a7d21399ec964dc3bd661b5d83b4bbda480ff
ramananda <ramanandasamantaray@gmail.com>
2024-04-30 22:39:11 +0530

    Changed Api endpoints

--- src/apiServices/petApiService.js
+++ src/apiServices/petApiService.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const petApi = createApi({
   reducerPath: "petApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://pets-v2.dev-apis.com" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://pets-v2.dev-apis.com" }),
   endpoints: (builder) => ({
     getPet: builder.query({
       query: (id) => ({ url: "pets", params: { id } }),
